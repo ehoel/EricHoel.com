8df8d807ed2b21c242ba92c8916e0903d723766b
= <=>
2016-04-21 00:44:01 -0500

    Added semicolon after ip

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var router = express.Router();  //Built in middle layer routing server provided 
 var path = __dirname + '/views/';  //__dirname points to current directory
                                     //Views store HTML. Used in Router.get call
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;  //uses default port on Openshift, or 3000 locally
-var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1' //added from https://blog.openshift.com/run-your-nodejs-projects-on-openshift-in-two-simple-steps/
+var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'; //added from https://blog.openshift.com/run-your-nodejs-projects-on-openshift-in-two-simple-steps/
 
 router.use(function (req,res,next) {
   console.log("/" + req.method);  //Log to console.
@@ -33,7 +33,7 @@ app.use("/",router);  //Tells Express to Use the routes we defined above.
 //  res.sendFile(path + "404.html");  //This will execute and host a 404 page.
 //});
 
-app.listen(port,function(){  //Listen on port 3000
+app.listen(port,ip,function(){  //Listen on port 3000
     console.log((new Date()) + " Listening on " + ip + ", server_port " + port); 
     //console.log("Live at Port 3000");
 });
